38793229e07c62d65598c022c8e05164e4d5577c
eddytim <ahmedtheidealman@gmail.com>
2021-04-30 16:59:46 +0300

    removed applicationId and changed com.android.application to com.android.library in gradle

---
+++ app/src/main/java/org/tensorflow/lite/examples/classification/TestProcess.java
@@ -0,0 +1,10 @@
+package org.tensorflow.lite.examples.classification;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class TestProcess {
+    public static void toast(Context context){
+        Toast.makeText(context, "Hello there", Toast.LENGTH_SHORT).show();
+    }
+}
